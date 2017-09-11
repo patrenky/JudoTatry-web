c91d186edcc5df76d4afaf2f6aff71e502b82229
Patrik Michalák <michalak@inqool.cz>
2017-09-11 08:49:45 +0200

    production update

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Waypoint from "react-waypoint";
 import ScrollToTop from "./components/ScrollToTop";
 import Menu from "./components/Menu";
 import Main from "./containers/Main";
-import News from "./containers/News";
+// import News from "./containers/News";
 import About from "./containers/About";
 import Contact from "./containers/Contact";
-import Admin from "./containers/Admin";
+// import Admin from "./containers/Admin";
 
 const App = ({ store, pinnedHeader, pinHeader }) => {
   return (
@@ -25,10 +25,10 @@ const App = ({ store, pinnedHeader, pinHeader }) => {
               topOffset={-100}
             />
             <Route exact path="/" component={Main} />
-            <Route path="/aktuality" component={News} />
+            <Route path="/aktuality" component={Main} />
             <Route path="/judo" component={About} />
             <Route path="/kontakt" component={Contact} />
-            <Route path="/admin" component={Admin} />
+            <Route path="/admin" component={Main} />
           </div>
         </ScrollToTop>
       </Router>
